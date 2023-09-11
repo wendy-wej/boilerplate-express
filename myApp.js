08569ff994912ea9e46a27517e68d106ce446b04
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-09-11 11:55:54 +0100

    changes

--- myApp.js
+++ myApp.js
@@ -15,12 +15,12 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", (req,res) =>{
     if(process.env.MESSAGE_STYLE==="uppercase"){
         const response = "HELLO JSON"
-        
+        res.json({"message": response})
     } else {
         const response = "Hello json"
-        
+        res.json({"message": response})
     }
-    res.json({"message": response})
+        
     });
 
 
