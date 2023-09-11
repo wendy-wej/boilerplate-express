4a7cfea2c9a1e01a8e77da4b4c11a37f62fd8c28
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-09-11 16:20:19 +0100

    chaining middleware

--- myApp.js
+++ myApp.js
@@ -25,7 +25,12 @@ app.get("/json", (req, res) => {
     res.json({"message": message})
 });
 
-
+app.get("/now", (req, res, next) =>{
+    req.time = new Date().toString()
+    next()
+}, (req, res) => {
+    res.json({"time": req.time})
+})
 
 
 
